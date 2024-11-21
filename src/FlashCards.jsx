204a6911e7ddc6629043fbd7db44ccff31c29c93
Artem Trifonov <artemis.levitt@gmail.com>
2024-11-21 13:12:03 +0100

    Implement app logic

--- src/FlashCards.jsx
+++ src/FlashCards.jsx
@@ -1,10 +1,17 @@
 import questions from "./questions.js";
+import {useState} from "react";
 
 function FlashCards() {
+    const [selectedId, setSelectedId] = useState(null);
     return (
         <div className="flashcards">{questions.map(question => (
-            <div key={question.id}>
-                <p>{question.question}</p>
+            <div
+                key={question.id}
+                className={question.id === selectedId ? 'selected' : ''}
+                onClick={()=>setSelectedId(question.id)}>
+                <p>
+                    {question.id === selectedId ? question.answer : question.question}
+                </p>
             </div>
         ))}</div>
     )
